53a03c9410c97a879b64597a12cb7c2404b8ee98
Sven Olafsen <stockburner9@gmail.com>
2024-05-13 11:23:11 -0500

    updated logged out  header

--- src/components/LandingNeederMapScreen.jsx
+++ src/components/LandingNeederMapScreen.jsx
@@ -191,7 +191,8 @@ const LandingNeederMapScreen = () => {
   return (
     <div>
       <APIProvider apiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
-        <Box h={{base: "50vh", lg: "70vh"}} w={{base: "90vw", lg: "35vw"}}>
+        <Box h={{base: "50vh", lg: "70vh"}} w={{base: "90vw", md: "30vw", lg: "35vw"}}>
+      
           <Map
             defaultCenter={{ lat: 44.89929301068098, lng: -93.3413753387615}}
             defaultZoom={12}
@@ -308,8 +309,10 @@ const LandingNeederMapScreen = () => {
                 </AdvancedMarker>
               ))}
           </Map>
+         
         </Box>
       </APIProvider>
+      
     </div>
   );
 };

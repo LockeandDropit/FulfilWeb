019c91dff3952fdf21341ff85d93ab7f9987d08b
Sven Olafsen <stockburner9@gmail.com>
2024-05-20 10:32:59 -0500

    Doer Sidebar

--- src/components/Landing/MapHero.jsx
+++ src/components/Landing/MapHero.jsx
@@ -1,44 +1,88 @@
 import React from "react";
 import LandingNeederMapScreen from "../LandingNeederMapScreen";
 import { useNavigate } from "react-router-dom";
+import { useMediaQuery } from "@chakra-ui/react";
+
+
 
 const MapHero = () => {
+
+
+  const [isDesktop] = useMediaQuery("(min-width: 500px)");
   const navigate = useNavigate();
   return (
-    <div class="mt-20 max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 flex lg:items-center items-center md:items-center">
-      <div class="w-full max-w-[50rem]rounded-lg ml-6">  <LandingNeederMapScreen /></div>
-    
+<>
+{isDesktop ? (<div class="mt-20 max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 flex lg:items-center items-center md:items-center">
+<div class="w-full max-w-[50rem]rounded-lg ml-6">  <LandingNeederMapScreen /></div>
 
-      <div class="w-full justify-center">
-        <div class=" bg-white  mt-4 rounded-xl shadow-sm ">
-          <div class="p-4 sm:p-7">
-            <h1 class="block text-3xl font-bold text-gray-800 sm:text-2xl md:text-3xl lg:text-4xl">
-              Know what you need?{" "}
-            </h1>
-            <p class="mt-3 text-lg text-gray-800">
-              Post what you need done and let the professionals come to you
-            </p>
 
-            <div class="mt-5 lg:mt-8 ">
-              <div class="w-full sm:w-auto">
-                <label for="hero-input" class="sr-only">
-                  Search
-                </label>
-                {/* <input type="text" id="hero-input" name="hero-input" class="py-3 px-4 block w-full xl:min-w-72 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter work email"> */}
-              </div>
-              <button
-                class="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-sky-400 text-white hover:bg-sky-600 disabled:opacity-50 disabled:pointer-events-none"
-                onClick={() => navigate("/NeederEmailRegister")}
-              >
-                Post my job
-              </button>
-            </div>
-          </div>
+<div class="w-full justify-center">
+  <div class=" bg-white  mt-4 rounded-xl shadow-sm ">
+    <div class="p-4 sm:p-7">
+      <h1 class="block text-3xl font-bold text-gray-800 sm:text-2xl md:text-3xl lg:text-4xl">
+        Know what you need?{" "}
+      </h1>
+      <p class="mt-3 text-lg text-gray-800">
+        Post what you need done and let the professionals come to you
+      </p>
+
+      <div class="mt-5 lg:mt-8 ">
+        <div class="w-full sm:w-auto">
+          <label for="hero-input" class="sr-only">
+            Search
+          </label>
+          {/* <input type="text" id="hero-input" name="hero-input" class="py-3 px-4 block w-full xl:min-w-72 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter work email"> */}
         </div>
+        <button
+          class="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-sky-400 text-white hover:bg-sky-600 disabled:opacity-50 disabled:pointer-events-none"
+          onClick={() => navigate("/NeederEmailRegister")}
+        >
+          Post my job
+        </button>
       </div>
-
-     
     </div>
+  </div>
+</div>
+
+
+</div>) : (
+<div>
+<div class="mt-20 max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 flex lg:items-center items-center md:items-center">
+<div class="w-full justify-center">
+  <div class=" bg-white  mt-4 rounded-xl shadow-sm ">
+    <div class="p-4 sm:p-7">
+      <h1 class="block text-3xl font-bold text-gray-800 sm:text-2xl md:text-3xl lg:text-4xl">
+        Know what you need?{" "}
+      </h1>
+      <p class="mt-3 text-lg text-gray-800">
+        Post what you need done and let the professionals come to you
+      </p>
+
+      <div class="mt-5 lg:mt-8 ">
+        <div class="w-full sm:w-auto">
+          <label for="hero-input" class="sr-only">
+            Search
+          </label>
+          {/* <input type="text" id="hero-input" name="hero-input" class="py-3 px-4 block w-full xl:min-w-72 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter work email"> */}
+        </div>
+        <button
+          class="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-sky-400 text-white hover:bg-sky-600 disabled:opacity-50 disabled:pointer-events-none"
+          onClick={() => navigate("/NeederEmailRegister")}
+        >
+          Post my job
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</div>
+{/* <div class="mt-20 max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 flex lg:items-center items-center md:items-center">
+<div class="w-full rounded-lg ml-6">  <LandingNeederMapScreen /></div>
+</div> */}
+</div>)}
+    </>
   );
 };
 
--- src/pages/Doer/components/Header.jsx
+++ src/pages/Doer/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { onAuthStateChanged, signOut, getAuth } from "firebase/auth";
 import { auth, logout, db } from "../../../firebaseConfig";
 import { useState, useEffect } from "react";
-import { query, collection, onSnapshot, getDoc, doc } from "firebase/firestore";
+import { query, collection, onSnapshot, getDoc, doc, updateDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 
 import {
@@ -36,8 +36,12 @@ import {
   ListIcon,
   ListItem,
   VStack,
-  Text
+  Text,
+  Box,
+  Heading
 } from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
+
 
 const Header = () => {
     const [user, setUser] = useState(null);
@@ -109,12 +113,89 @@ const Header = () => {
 
     console.log(profilePicture)
 
+    
+
+  //laoding control
+
+  const [loading, setLoading] = useState(true);
+  setTimeout(() => {
+    setLoading(false);
+  }, 1000);
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [isPremium, setIsPremium] = useState(null)
+
+  const [test, setTest] = useState("test")
+
+  useEffect(() => {
+    if (user) {
+      const docRef = doc(db, "users", user.uid);
+      getDoc(docRef).then((snapshot) => {
+     console.log(snapshot.data())
+        setIsPremium(snapshot.data().isPremium)
+      })
+        .then(() => {
+          setTimeout(() => {
+            setIsLoading(false)
+          }, 500)
+        
+        })
+        .catch((error) => {
+          // no buen
+          console.log(error)
+        });
+    }
+  }, [user]);
 
 
+  useEffect(() => {
+    console.log(isLoading, isPremium)
+  }, [isLoading, isPremium])
+
+
+
+  const [subscriptionID, setSubscriptionID] = useState(null);
+
+  const initializeSubscription = () => {
+    //credit and help from https://github.com/pagecow/stripe-subscribe-payments
+    fetch("https://fulfil-api.onrender.com/create-subscription-session", {
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then(({ session }) => {
+        setSubscriptionID(session.id);
+        window.open(session.url, "_blank");
+      })
+      // .then(({ url }) => {
+      //   // window.location = url
+      //   window.open(url, "_blank")
+      // })
+      .catch((e) => {
+        console.error(e.error);
+      });
+  };
+
+  useEffect(() => {
+    if (subscriptionID) {
+      updateDoc(doc(db, "users", user.uid), {
+        subscriptionID: subscriptionID,
+      })
+        .then(() => {
+          //all good
+        })
+        .catch((error) => {
+          // no bueno
+        });
+    }
+  }, [subscriptionID]);
+
+
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
  
  const [showDropdown, setShowDropdown] = useState(false)
  
- 
+ const { isOpen: isOPenMobileDash, onOpen: onOpenMobileDash, onClose: onCloseMobileDash } = useDisclosure()
         
 
   return (
@@ -124,7 +205,7 @@ const Header = () => {
       <div class="xl:col-span-1 flex items-center md:gap-x-3">
         <div class="lg:hidden">
         
-          <button type="button" class="w-7 h-[38px] inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none" data-hs-overlay="#hs-pro-sidebar" aria-controls="hs-pro-sidebar" aria-label="Toggle navigation">
+          <button type="button" onClick={() => onOpenMobileDash()}class="w-7 h-[38px] inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none" data-hs-overlay="#hs-pro-sidebar" aria-controls="hs-pro-sidebar" aria-label="Toggle navigation">
             <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
           </button>
          
@@ -636,7 +717,169 @@ const Header = () => {
              
 
             
-  
+        <Drawer
+        isOpen={isOPenMobileDash}
+        placement='left'
+        onClose={onCloseMobileDash}
+        
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerBody>
+          <div class="flex flex-col h-full max-h-full py-3">
+          <header class="h-[46px] px-8">
+            <a
+              class="flex-none text-4xl font-sans font-bold text-sky-400"
+              aria-label="Brand"
+              onClick={() => navigate("/DoerMapScreen")}
+            >
+              Fulfil
+            </a>
+          </header>
+
+          <div class="h-[calc(100%-35px)] lg:h-[calc(100%-93px)] overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300">
+            <nav
+              class="hs-accordion-group pb-3  w-full flex flex-col flex-wrap"
+              data-hs-accordion-always-open
+            >
+              <ul>
+                <li class="px-5 mb-1.5">
+                  <button
+                    class="flex gap-x-3 py-2 px-3 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 "
+                    onClick={() => navigate("/DoerMapScreen")}
+                  >
+                    <svg
+                      class="flex-shrink-0 mt-0.5 size-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                      <polyline points="9 22 9 12 15 12 15 22" />
+                    </svg>
+                    My Jobs
+                  </button>
+                </li>
+
+                <button
+                  type="button"
+                  class="hs-accordion-toggle px-8 mb-1.5 hs-accordion-active:bg-gray-100 w-full text-start flex gap-x-3 py-2 px-3 text-sm text-gray-800 rounded-lg hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100"
+                  onClick={() => navigate("/DoerMessageList")}
+                >
+                  <svg
+                    class="flex-shrink-0 mt-0.5 size-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                    <circle cx="9" cy="7" r="4" />
+                    <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+                    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+                  </svg>
+                  Messages
+                </button>
+
+                <button
+                  type="button"
+                  onClick={() => {
+                    navigate("/UserProfile");
+                  }}
+                  class="hs-accordion-toggle  px-8 mb-1.5 hs-accordion-active:bg-gray-100 w-full text-start flex gap-x-3 py-2 px-3 text-sm text-gray-800 rounded-lg hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100"
+                >
+                  <svg
+                    class="flex-shrink-0 mt-0.5 size-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <circle cx="12" cy="12" r="10" />
+                    <circle cx="12" cy="10" r="3" />
+                    <path d="M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662" />
+                  </svg>
+                  My Profile
+                </button>
+
+                <button
+                  type="button"
+                  onClick={() => navigate("/DoerAccountManager")}
+                  class="hs-accordion-toggle px-8 mb-1.5 hs-accordion-active:bg-gray-100 w-full text-start flex gap-x-3 py-2 px-3 text-sm text-gray-800 rounded-lg hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100"
+                >
+                  <svg
+                    class="flex-shrink-0 mt-0.5 size-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <circle cx="18" cy="15" r="3" />
+                    <circle cx="9" cy="7" r="4" />
+                    <path d="M10 15H6a4 4 0 0 0-4 4v2" />
+                    <path d="m21.7 16.4-.9-.3" />
+                    <path d="m15.2 13.9-.9-.3" />
+                    <path d="m16.6 18.7.3-.9" />
+                    <path d="m19.1 12.2.3-.9" />
+                    <path d="m19.6 18.7-.4-1" />
+                    <path d="m16.8 12.3-.4-1" />
+                    <path d="m14.3 16.6 1-.4" />
+                    <path d="m20.7 13.8 1-.4" />
+                  </svg>
+                  Account Settings
+                </button>
+
+                <li class="pt-5 px-8 mt-5 mb-1.5 border-t border-gray-200 first:border-transparent first:pt-0">
+                  <span class="block text-xs uppercase text-gray-500">
+                    Actions
+                  </span>
+                </li>
+
+              
+                <li class="px-8 mb-0.5 mt-10">
+                  <button
+                    type="button"
+                    class="py-2  px-3 inline-flex text-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-sky-400 text-white hover:bg-sky-500 disabled:opacity-50 disabled:pointer-events-none"
+                   onClick={() => onOpen()}
+                  >
+                    
+                    Upgrade To Pro
+                  </button>
+                 
+                </li>
+
+                
+              
+              </ul>
+            </nav>
+          </div>
+          </div>
+      
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
           
          
        
@@ -645,6 +888,90 @@ const Header = () => {
         </div>
       </div>
     </div>
+    <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <Center py={6}>
+            <Box
+              maxW={"330px"}
+              w={"full"}
+              bg={useColorModeValue("white", "gray.800")}
+              rounded={"md"}
+              overflow={"hidden"}
+            >
+              <VStack spacing={1} textAlign="center">
+                <Heading as="h1" fontSize="4xl">
+                  Upgrade to premium
+                </Heading>
+                <Text fontSize="lg" color={"gray.500"}>
+                  Get the frist 2 months for $1/month. Then continue at
+                  $29/month.
+                </Text>
+
+                <Text fontSize="md" color={"gray.500"}>
+                  Cancel at anytime.
+                </Text>
+              </VStack>
+              <Stack
+                textAlign={"center"}
+                p={5}
+                color={useColorModeValue("gray.800", "white")}
+                align={"center"}
+              >
+                <Text
+                  fontSize={"md"}
+                  fontWeight={500}
+                  textColor="#01A2E8"
+                  p={2}
+                  px={3}
+                  rounded={"full"}
+                >
+                  Premium Subscription
+                </Text>
+                <Stack direction={"row"} align={"center"} justify={"center"}>
+                  <Text fontSize={"3xl"}>$</Text>
+                  <Text fontSize={"6xl"} fontWeight={800}>
+                    1
+                  </Text>
+                  <Text color={"gray.500"}>/month</Text>
+                </Stack>
+              </Stack>
+
+              <Box px={1} py={6}>
+                <List spacing={3}>
+                  <ListItem>
+                    <ListIcon as={CheckIcon} color="#01A2E8" />
+                    Save 50% on all transaction fees
+                  </ListItem>
+                  <ListItem>
+                    <ListIcon as={CheckIcon} color="#01A2E8" />
+                    Get noticed by customers as a Premium Contractor
+                  </ListItem>
+
+                  <ListItem>
+                    <ListIcon as={CheckIcon} color="#01A2E8" />
+                    Be seen by customers who are looking for contractors in your
+                    category
+                  </ListItem>
+                </List>
+
+                <Button
+                  mt={10}
+                  w={"full"}
+                  bg="#01A2E8"
+                  color={"white"}
+                  rounded={"xl"}
+                  boxShadow={"0 5px 20px 0px rgb(72 187 120 / 43%)"}
+                  _hover={{ bg: "#018ecb", textColor: "white" }}
+                  onClick={() => initializeSubscription()}
+                >
+                  Start your trial
+                </Button>
+              </Box>
+            </Box>
+          </Center>
+        </ModalContent>
+      </Modal>
   </header>
   </>
   )

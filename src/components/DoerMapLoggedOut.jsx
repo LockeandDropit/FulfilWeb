81a93594674b0c7daeaf24c2101af44bfd617462
Sven Olafsen <stockburner9@gmail.com>
2024-06-20 14:44:29 -0500

    Emails

--- src/components/DoerMapLoggedOut.jsx
+++ src/components/DoerMapLoggedOut.jsx
@@ -1642,7 +1642,7 @@ console.log("session id ", sessionId)
       <div>
         <div className="flex mt-2">
       <div class="w-full space-y-3">
-  <input type="text" onChange={(e) => setSubscriberEmail(e.traget.event)} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none " placeholder="Enter your email here" />
+  <input type="text" onChange={(e) => setSubscriberEmail(e.target.event)} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none " placeholder="Enter your email here" />
 </div>
         <button type="button"   class="w-1/4 ml-1 py-1 px-2  items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-500 text-white hover:bg-blue-600 disabled:opacity-50 disabled:pointer-events-none" onClick={() => handleNewEmailSignUp()}>
           <span >Stay Notified</span>

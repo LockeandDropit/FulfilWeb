7eb03353df3d5c21d71a2de9e92233ef462f4152
Sven Olafsen <stockburner9@gmail.com>
2024-10-17 16:53:54 -0500

    landing page update, graph added

--- src/components/Landing/CostWithChart.jsx
+++ src/components/Landing/CostWithChart.jsx
@@ -5,22 +5,27 @@ const CostWithChart = () => {
   const navigate = useNavigate();
   return (
    
+    <div className='w-full bg-sky-100 mt-10 py-10'>
 
-    <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto sm:mt-20 min-h-[500px] ">
-      <div class="grid md:grid-cols-2 gap-12  min-h-[500px] ">
-       {/* //chart here */}
 
-      <div className='flex '>
-      <BarChartComponent />
-      </div>
+
+    <div class="max-w-[85rem] mx-auto bg-sky-100 flex align-center items-center justify-center  ">
+
+
+    {/* <img src="/landingImages/CanvaGraph3.png" alt="" className="mt-20" /> */}
+      
+      <div class="grid md:grid-cols-2 gap-12   ">
+      
+
+ 
        
  
     
-        <div class="flex align-text items-center justify-center space-y-6 lg:space-y-10 ">
+        <div class="flex  space-y-6 lg:space-y-10 mt-10">
         
         <div class="lg:w-3/4">
           <h2 class="text-3xl text-gray-800 font-bold lg:text-4xl ">
-            Get more out of your time and money.
+           What's the cost?
           </h2>
           <p class="mt-5 text-gray-800 text-lg ">
             The average total cost of a technical school in Minnesota is $9000 while the average cost of a traditional 4-year college is $40,000 <span className='text-xs position-top'>(1), (4)</span>
@@ -31,10 +36,7 @@ const CostWithChart = () => {
           <p class="mt-3 text-gray-800 text-lg"> A graduate from a 4 year college will start at $55,000, only $2,000 more<span className='text-xs position-top'>(2)</span>.</p>
          
           <p class="mt-5">
-            {/* <a class="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500" href="#">
-              Contact sales to learn more
-              <svg class="shrink-0 size-4 transition ease-in-out group-hover:translate-x-1 group-focus:translate-x-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
-            </a> */}
+          
           </p>
 
           <p className='mt-12 text-xs text-gray-800' >Sources:</p>
@@ -45,12 +47,16 @@ const CostWithChart = () => {
            
         </div>
         </div>
+
+        <div className='flex '>
+      <img src="/landingImages/CanvaGraph3.png" />
+      </div>
      
       </div>
     
     </div>
 
-  
+    </div>
   )
 }
 

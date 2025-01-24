9d3ae5b8e066d92d78e9377ec1b99d788595377f
Sven Olafsen <stockburner9@gmail.com>
2025-01-24 12:35:08 -0600

    added a different hompage dashboard

--- src/pages/Register/Doer/NewOnboarding/CareerInterests.jsx
+++ src/pages/Register/Doer/NewOnboarding/CareerInterests.jsx
@@ -79,15 +79,7 @@ const uploadAnswer = async () => {
         <div class="w-full space-y-3 flex items-center justify-center">
   <textarea onChange={(e) => setInterests(e.target.value)} class=" mt-5 sm:mt-6 py-3 px-4 block w-full sm:w-3/4  border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" rows="4" placeholder="e.g. I like to work with my hands and be outside. I like to work with people and problem solve."></textarea>
 </div>
-        {interests ? (
-          <button
-            type="button"
-            class=" w-full sm:w-1/2 text-center justify-center mt-6 lg:mt-10 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-sky-400 text-white hover:bg-sky-500 focus:outline-none  disabled:opacity-50 disabled:pointer-events-none"
-            onClick={getRecommendation}
-         >
-            Finish
-          </button>
-        ) : loading ? (
+        {loading ? (
           <button
             type="button"
             class=" w-full sm:w-1/2 text-center justify-center mt-6 lg:mt-10 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-sky-400 text-white hover:bg-sky-500 focus:outline-none  disabled:opacity-50 disabled:pointer-events-none"
@@ -97,7 +89,15 @@ const uploadAnswer = async () => {
   <span className="sr-only">Loading...</span>
 </div>
           </button>
-        ) : (
+        ) :  interests ? (
+          <button
+            type="button"
+            class=" w-full sm:w-1/2 text-center justify-center mt-6 lg:mt-10 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-sky-400 text-white hover:bg-sky-500 focus:outline-none  disabled:opacity-50 disabled:pointer-events-none"
+            onClick={getRecommendation}
+         >
+            Finish
+          </button>
+        )  : (
           <button
             type="button"
             class=" w-full sm:w-1/2 text-center justify-center mt-6 lg:mt-10 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-sky-200 text-white hove focus:outline-none  pointer-events-none disabled:opacity-50 disabled:pointer-events-none"

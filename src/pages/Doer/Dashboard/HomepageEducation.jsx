2e80758981721817d56a97c34bdc7f485072e9e3
Sven Olafsen <stockburner9@gmail.com>
2025-01-31 10:48:31 -0600

    added refresh to edu section

--- src/pages/Doer/Dashboard/HomepageEducation.jsx
+++ src/pages/Doer/Dashboard/HomepageEducation.jsx
@@ -1,22 +1,13 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { db } from "../../../firebaseConfig";
-import { getDoc, doc } from "firebase/firestore";
-const HomepageEducation = ({user}) => {
+import { getDoc, doc, updateDoc } from "firebase/firestore";
+const HomepageEducation = ({ user }) => {
   const [userResumeInformation, setUserResumeInformation] = useState(null);
 
-
   const [returnedJobs, setReturnedJobs] = useState(null);
+  const [newReturnedEdu, setNewReturnedEdu] = useState(null);
   const [loading, setLoading] = useState(true);
-
-  // const [city, setCity] = useState("minneapolis");
-  // const [state, setState] = useState("MN");
-  // const [currentPay, setCurrentPay] = useState("$37 an hour");
-  // const [currentPayType, setCurrentPayType] = useState(null);
-  // const [fieldsOfInterest, setFieldsOfInterest] = useState(
-  //   "working with machines, working with peopl, welding"
-  // );
-  // const [interestFreeType, setInterestFreeType] = useState(null);
 
   const getEdu = async () => {
     setLoading(true);
@@ -39,27 +30,37 @@ const HomepageEducation = ({user}) => {
     console.log("json resopnse w array EDU", JSON.parse(json.message.content));
 
     setReturnedJobs(JSON.parse(json.message.content));
+    setNewReturnedEdu(JSON.parse(json.message.content));
     // setLoading(false);
   };
 
+  useEffect(() => {
+    if (user != null) {
+      const docRef = doc(db, "users", user.uid);
 
+      getDoc(docRef).then((snapshot) => {
+        // console.log(snapshot.data());
+        setReturnedJobs(snapshot.data().returnedEducation);
+      });
+    } else {
+      console.log("oops!");
+    }
+  }, [user]);
+
+
+
+  const uploadEdu = async () => {
+    await updateDoc(doc(db, "users", user.uid), {
+      returnedEducation: newReturnedEdu,
+    });
+  };
 
   useEffect(() => {
-    // getEdu();
-  }, []);
-
-    useEffect(() => {
-        if (user != null) {
-          const docRef = doc(db, "users", user.uid);
-    
-          getDoc(docRef).then((snapshot) => {
-            // console.log(snapshot.data());
-           setReturnedJobs(snapshot.data().returnedEducation)
-          });
-        } else {
-          console.log("oops!");
-        }
-      }, [user]);
+    if (newReturnedEdu) {
+      //upload to firebase
+      uploadEdu();
+    }
+  }, [newReturnedEdu]);
 
   useEffect(() => {
     if (returnedJobs) {
@@ -74,8 +75,8 @@ const HomepageEducation = ({user}) => {
   return (
     <div className="w-full  py-6 py-12 mb-16">
       <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 ">
-        <div class="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center ">
-          <div className="">
+        <div className="flex flex-row w-full">
+          <div>
             <h1 class="block text-3xl font-semibold text-gray-800 sm:text-2xl lg:text-3xl lg:leading-tight ">
               Education & Training
             </h1>
@@ -84,16 +85,32 @@ const HomepageEducation = ({user}) => {
               potential.
             </p>
           </div>
+          {loading ? (
+            <button className="border border-gray-300 px-4 py-1 h-2/3 ml-auto bg-gray-100 hover:bg-gray-200 text-gray-800 font-medium rounded-md shadow-sm">
+              <div
+                className="animate-spin inline-block size-4 border-[3px] border-current border-t-transparent text-gray-700 rounded-full"
+                role="status"
+                aria-label="loading"
+              >
+                <span className="sr-only">Loading...</span>
+              </div>
+            </button>
+          ) : (
+            <button className="border border-gray-300 px-4 py-1 h-2/3 ml-auto bg-gray-100 hover:bg-gray-200 text-gray-800 text-sm font-medium rounded-md shadow-sm"
+            onClick={() => getEdu()}
+            >
+              Refresh
+            </button>
+          )}
         </div>
 
         <div className="flex flex-col sm:flex-row mt-4 md:mt-6">
           {returnedJobs?.map((edu) => (
-                       <div className="flex flex-col sm:flex-row mt-4 md:mt-6 p-1 w-full sm:w-1/3">
+            <div className="flex flex-col sm:flex-row mt-4 md:mt-6 p-1 w-full sm:w-1/3">
               <div class=" p-5 space-y-4 flex flex-col bg-white border border-gray-200 rounded-xl ">
                 <div class="flex justify-between">
                   <div class="flex flex-col justify-center items-center rounded-lg ">
-                  <span class="ml-1 inline-flex items-center gap-x-1 text-base font-medium text-green-600 rounded-full">
-                     
+                    <span class="ml-1 inline-flex items-center gap-x-1 text-base font-medium text-green-600 rounded-full">
                       {edu.percent_increase}% pay increase
                       <svg
                         class="shrink-0 size-4"
@@ -120,9 +137,11 @@ const HomepageEducation = ({user}) => {
                   <h3 class="text-sm text-gray-500">{edu.location}</h3>
                   <div className="mt-1 flex ">
                     <h3 class="text-sm text-gray-500">
-                      <p className="mr-1 font-medium text-sm text-gray-800">Avg. Salary:</p> ${edu.salary_after_completion}
+                      <p className="mr-1 font-medium text-sm text-gray-800">
+                        Avg. Salary:
+                      </p>{" "}
+                      ${edu.salary_after_completion}
                     </h3>
-                 
                   </div>
                   <p class="mt-3 text-gray-700 line-clamp-4 ">
                     {edu.description}
@@ -136,14 +155,8 @@ const HomepageEducation = ({user}) => {
                   >
                     Save
                   </button>
-                  {/* <button
-                    type="button"
-                    class="ml-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 "
-                    onClick={() => handleOpenJob(edu)}
-                  >
-                    Apply
-                  </button> */}
-                    <button
+            
+                  <button
                     type="button"
                     class="ml-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg  bg-sky-400 text-white shadow-sm hover:bg-sky-500 "
                     onClick={() => handleOpenJob(edu)}
@@ -155,122 +168,116 @@ const HomepageEducation = ({user}) => {
             </div>
           ))}
 
-{loading && (<div className="flex flex-col sm:flex-row sm:w-full"> 
-          <div className="flex flex-col sm:flex-row mt-4 md:mt-6 p-1 w-full sm:w-1/3">
-            <div class=" p-5 space-y-4 flex flex-col bg-white border border-gray-200 rounded-xl w-full ">
-              <div class="flex justify-between">
-                <div class="flex flex-col justify-center items-center size-[38px] border border-gray-200 rounded-lg ">
-                
+          {loading && (
+            <div className="flex flex-col sm:flex-row sm:w-full">
+              <div className="flex flex-col sm:flex-row mt-4 md:mt-6 p-1 w-full sm:w-1/3">
+                <div class=" p-5 space-y-4 flex flex-col bg-white border border-gray-200 rounded-xl w-full ">
+                  <div class="flex justify-between">
+                    <div class="flex flex-col justify-center items-center size-[38px] border border-gray-200 rounded-lg "></div>
+                  </div>
+
+                  <div className="flex flex-col animate-pulse">
+                    <ul class="w-1/3 h-4 bg-gray-400 rounded-full "></ul>
+                    <ul class="w-1/4 h-4 mt-1 bg-gray-400 rounded-full "></ul>
+                    <ul class="w-1/4 h-4 mt-1 bg-gray-400 rounded-full "></ul>
+
+                    <p class="mt-3 ">
+                      <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
+                      <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
+                      <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
+                      <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
+                    </p>
+                  </div>
+
+                  <div className="flex mt-auto mb-1">
+                    <button
+                      type="button"
+                      class="mr-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 "
+                    >
+                      Save
+                    </button>
+                    <button
+                      type="button"
+                      class="mr-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 "
+                    >
+                      Apply
+                    </button>
+                  </div>
                 </div>
               </div>
+              <div className="flex flex-row mt-4 md:mt-6 p-1 w-full sm:w-1/3">
+                <div class=" p-5 space-y-4 flex flex-col bg-white border border-gray-200 rounded-xl w-full">
+                  <div class="flex justify-between">
+                    <div class="flex flex-col justify-center items-center size-[38px] border border-gray-200 rounded-lg "></div>
+                  </div>
 
-              <div className="flex flex-col animate-pulse">
-                <ul class="w-1/3 h-4 bg-gray-400 rounded-full "></ul>
-                <ul class="w-1/4 h-4 mt-1 bg-gray-400 rounded-full "></ul>
-                <ul class="w-1/4 h-4 mt-1 bg-gray-400 rounded-full "></ul>
+                  <div className="flex flex-col animate-pulse">
+                    <ul class="w-1/3 h-4 bg-gray-400 rounded-full "></ul>
+                    <ul class="w-1/4 h-4 mt-1 bg-gray-400 rounded-full "></ul>
+                    <ul class="w-1/4 h-4 mt-1 bg-gray-400 rounded-full "></ul>
 
-                <p class="mt-3 ">
-                  <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
-                  <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
-                  <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
-                  <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
-                </p>
-              </div>
+                    <p class="mt-3 ">
+                      <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
+                      <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
+                      <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
+                      <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
+                    </p>
+                  </div>
 
-              <div className="flex mt-auto mb-1">
-                <button
-                  type="button"
-                  class="mr-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 "
-                >
-                  Save
-                </button>
-                <button
-                  type="button"
-                  class="mr-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 "
-                >
-                  Apply
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-row mt-4 md:mt-6 p-1 w-full sm:w-1/3">
-            <div class=" p-5 space-y-4 flex flex-col bg-white border border-gray-200 rounded-xl w-full">
-              <div class="flex justify-between">
-                <div class="flex flex-col justify-center items-center size-[38px] border border-gray-200 rounded-lg ">
-                 
+                  <div className="flex mt-auto mb-1">
+                    <button
+                      type="button"
+                      class="mr-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 "
+                    >
+                      Save
+                    </button>
+                    <button
+                      type="button"
+                      class="mr-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 "
+                    >
+                      Apply
+                    </button>
+                  </div>
                 </div>
               </div>
+              <div className="flex flex-row mt-4 md:mt-6 p-1 w-full sm:w-1/3">
+                <div class=" p-5 space-y-4 flex flex-col bg-white border border-gray-200 rounded-xl w-full">
+                  <div class="flex justify-between">
+                    <div class="flex flex-col justify-center items-center size-[38px] border border-gray-200 rounded-lg "></div>
+                  </div>
 
-              <div className="flex flex-col animate-pulse">
-                <ul class="w-1/3 h-4 bg-gray-400 rounded-full "></ul>
-                <ul class="w-1/4 h-4 mt-1 bg-gray-400 rounded-full "></ul>
-                <ul class="w-1/4 h-4 mt-1 bg-gray-400 rounded-full "></ul>
+                  <div className="flex flex-col animate-pulse">
+                    <ul class="w-1/3 h-4 bg-gray-400 rounded-full "></ul>
+                    <ul class="w-1/4 h-4 mt-1 bg-gray-400 rounded-full "></ul>
+                    <ul class="w-1/4 h-4 mt-1 bg-gray-400 rounded-full "></ul>
 
-                <p class="mt-3 ">
-                  <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
-                  <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
-                  <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
-                  <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
-                </p>
-              </div>
+                    <p class="mt-3 ">
+                      <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
+                      <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
+                      <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
+                      <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
+                    </p>
+                  </div>
 
-              <div className="flex mt-auto mb-1">
-                <button
-                  type="button"
-                  class="mr-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 "
-                >
-                  Save
-                </button>
-                <button
-                  type="button"
-                  class="mr-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 "
-                >
-                  Apply
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-row mt-4 md:mt-6 p-1 w-full sm:w-1/3">
-            <div class=" p-5 space-y-4 flex flex-col bg-white border border-gray-200 rounded-xl w-full">
-              <div class="flex justify-between">
-                <div class="flex flex-col justify-center items-center size-[38px] border border-gray-200 rounded-lg ">
-                
+                  <div className="flex mt-auto mb-1">
+                    <button
+                      type="button"
+                      class="mr-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 "
+                    >
+                      Save
+                    </button>
+                    <button
+                      type="button"
+                      class="mr-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 "
+                    >
+                      Apply
+                    </button>
+                  </div>
                 </div>
               </div>
-
-              <div className="flex flex-col animate-pulse">
-                <ul class="w-1/3 h-4 bg-gray-400 rounded-full "></ul>
-                <ul class="w-1/4 h-4 mt-1 bg-gray-400 rounded-full "></ul>
-                <ul class="w-1/4 h-4 mt-1 bg-gray-400 rounded-full "></ul>
-
-                <p class="mt-3 ">
-                  <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
-                  <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
-                  <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
-                  <ul class="w-full mt-1 h-4 bg-gray-400 rounded-full "></ul>
-                </p>
-              </div>
-
-              <div className="flex mt-auto mb-1">
-                <button
-                  type="button"
-                  class="mr-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 "
-                >
-                  Save
-                </button>
-                <button
-                  type="button"
-                  class="mr-1 py-2 px-3 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 "
-                >
-                  Apply
-                </button>
-              </div>
             </div>
-          </div>
-        </div>)}
-
-     
-     </div>
+          )}
+        </div>
       </div>
     </div>
   );
